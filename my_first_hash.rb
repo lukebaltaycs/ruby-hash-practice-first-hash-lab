ae976de14a2ebc000487b23ebf57aebc2e3ba837
Luke Baltay <luke.baltay@yale.edu>
2022-05-29 22:14:43 -0400

    commit tree bf7a792fc80f77a73d0661667323d8896234d8b7

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  my_hash = {"word" => "up"}
 end
 
 
@@ -22,6 +21,7 @@ def retrieval
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -32,12 +32,13 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+    shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
-
+    shipping_manifest["gun powder"] = 4
   # return the shipping_manifest hash below
+
+  shipping_manifest
 
 end
